d59bce61ab77662b4a1d3a05238a7736cbce72fe
Saeed-Alneyadi <122411428+Saeed-Alneyadi@users.noreply.github.com>
2026-09-16 08:28:48 +0400

    Publish FalconFlag website

---
+++ config.js
@@ -0,0 +1,2 @@
+// Public connection settings only. Never put a secret/service-role key here.
+window.FALCONFLAG_CONFIG = { supabaseUrl: 'https://yfrmibyudjmrfrztnqxn.supabase.co', supabasePublishableKey: 'sb_publishable_cFOeP1wdnihlppsFw0-OQg_e18wdQYU' };
